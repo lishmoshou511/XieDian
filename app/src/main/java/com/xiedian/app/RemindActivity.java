18c7906f7e0e6be87b207eb66815b0692c6c8a21
lishuang <lish516@126.com>
2014-07-03 01:05:13 +0800

    Init the git

---
+++ app/src/main/java/com/xiedian/app/RemindActivity.java
@@ -0,0 +1,52 @@
+package com.xiedian.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+
+
+public class RemindActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remind);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.remind, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+	//点击屏幕事件
+	@Override
+	public boolean onTouchEvent(android.view.MotionEvent event){
+
+		if(event.getAction()== MotionEvent.ACTION_UP){
+
+			Intent intent=new Intent(this,MapActivity.class);
+			startActivity(intent);
+		}
+
+		return super.onTouchEvent(event);
+	}
+}
